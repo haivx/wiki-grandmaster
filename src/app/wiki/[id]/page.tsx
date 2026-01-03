8d51778816fa5b2c3850b28f86f9c0b3d5e95fe7
Hai VX <haivx0206@gmail.com>
2026-01-03 11:12:28 +0700

    first init

---
+++ src/app/wiki/[id]/page.tsx
@@ -0,0 +1,41 @@
+import { notFound } from "next/navigation";
+import WikiArticleViewer from "@/components/wiki-article-viewer";
+import { authorizeUserToEditArticle } from "@/db/authz";
+import { getArticleById } from "@/lib/data/articles";
+import { stackServerApp } from "@/stack/server";
+
+interface ViewArticlePageProps {
+  params: Promise<{
+    id: string;
+  }>;
+}
+
+export default async function ViewArticlePage({
+  params,
+}: ViewArticlePageProps) {
+  const { id } = await params;
+
+  // Determine whether the currently-logged-in user can edit this article.
+  // If there's no logged-in user, `stackServerApp.getUser()` will return nullish
+  // and `canEdit` will be false.
+  let canEdit = false;
+  try {
+    const user = await stackServerApp.getUser();
+    if (user) {
+      console.log('user', user.id, id)
+      canEdit = await authorizeUserToEditArticle(user.id, +id);
+      console.log('can edit', canEdit)
+    }
+  } catch (_err) {
+    // On error, default to not allowing edits. Keeps behavior safe.
+    canEdit = false;
+  }
+
+  const article = await getArticleById(+id);
+
+  if (!article) {
+    notFound();
+  }
+
+  return <WikiArticleViewer article={article} canEdit={canEdit} />;
+}
